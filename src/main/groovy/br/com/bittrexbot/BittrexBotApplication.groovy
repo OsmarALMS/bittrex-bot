a3fa80759eb5951ddaacaeb0d7c5b3d0e165334c
Rafael Okada <rafael.okada@pismo.com.br>
2018-01-09 09:07:31 -0200

    gradle and groovy migration

---
+++ src/main/groovy/br/com/bittrexbot/BittrexBotApplication.groovy
@@ -0,0 +1,12 @@
+package br.com.bittrexbot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BittrexBotApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BittrexBotApplication, args
+	}
+}
