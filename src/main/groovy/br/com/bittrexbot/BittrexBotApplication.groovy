9d621e259bb9b021ec598219e3bf52014715c804
Rafael Yukio Okada <rafokada@gmail.com>
2018-01-09 23:23:13 -0200

    some refactor

--- src/main/groovy/br/com/bittrexbot/BittrexBotApplication.groovy
+++ src/main/groovy/br/com/bittrexbot/BittrexBotApplication.groovy
@@ -2,8 +2,10 @@ package br.com.bittrexbot
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class BittrexBotApplication {
 
 	static void main(String[] args) {
